f5f75aa934604636eda1e03d26d281ca0bdeb472
Priyobroto Kar <priyobrotokar@gmail.com>
2023-09-24 11:25:59 +0530

    fix: removed the deleted file import from the homepage

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,8 +1,5 @@
 import HomeBanner from "@/components/HomeBanner";
-import NavLinks from "@/components/NavLinks";
 import RecentlyPlayed from "@/components/RecentlyPlayed";
-import { Input } from "@/components/ui/input";
-import { FiSearch } from "react-icons/fi";
 
 export default function Home() {
   return (
